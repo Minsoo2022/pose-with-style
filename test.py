518ba83fd708a886859521849ba3920154b5b6f8
Minsoo2022 <54705353+Minsoo2022@users.noreply.github.com>
2022-04-04 16:51:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -144,6 +144,7 @@ def generate(args, loader, g_ema, device):
         real_img = data['target_image'].float().to(device)
         flow = data['flow'].float().to(device)
         sil = data['target_sil'].float().to(device)
+        feature = data['feature'].float().to(device)
         pred_img_ori = data['pred_image'].float().to(device)
         # attention = data['attention'].float().to(device)
         # attention = F.interpolate(attention, size=(args.size, args.size), mode='bilinear', align_corners=True)
@@ -189,7 +190,7 @@ def generate(args, loader, g_ema, device):
         with torch.no_grad():
             g_ema.eval()
             sample, _ = g_ema(appearance=appearance, flow=flow,
-                              sil=sil, input_feat=pred_img_ori,
+                              sil=sil, input_img=pred_img_ori, input_feat=feature,
                               condition=pred_img
                               )
 
